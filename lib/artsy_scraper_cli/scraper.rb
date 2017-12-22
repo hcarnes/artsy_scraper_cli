37e103ebba4b956cd9be3672e2f20a17cc5772b2
Hannah Carnes <hecarnes@gmail.com>
2017-12-22 17:31:13 -0500

    wait until selected gene is loaded

--- lib/artsy_scraper_cli/scraper.rb
+++ lib/artsy_scraper_cli/scraper.rb
@@ -15,6 +15,12 @@ class Scraper
   
   def scrape_gene_artworks(gene)
     gene.element.click
+    #wait for page to load 
+    while @driver.find_elements(css: ".cf-artworks[data-loading='true']").any?
+      #wait before checking again
+      puts "The list of #{gene.name} artworks is loading..."
+      sleep 0.1
+    end
     art_elements = @driver.find_elements(css: ".artwork-item")
     art_elements.map do |art_element|
       title = art_element.find_element(css: ".artwork-item-title").text
